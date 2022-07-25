66d5bba9e145285066a35c7af3a739cde1acda2f
Hristo <82899133+Foxyball@users.noreply.github.com>
2022-07-25 21:09:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-  //
+  <?php
+//
   $stmt1 = $conn->prepare("SELECT product_name FROM products WHERE  product_id = ? ");
   $stmt1->bind_param('i',$product_id);
   $stmt1->execute();
@@ -6,18 +7,19 @@
   $stmt1->bind_result($product_name);
   $stmt1->fetch();
 
-then
+//then
 
 echo $product_name;
 
-//
-METHOD WITH GET_RESULT(); 
+//METHOD WITH GET_RESULT(); 
 
   $stmt1 = $conn->prepare("SELECT product_name FROM products WHERE  product_id = ? ");
   $stmt1->bind_param('i',$product_id);
   $stmt1->execute();
   $title = $stmt1->get_result()->fetch_assoc();
 
-then 
+//then 
 
 echo $title['product_name'];
+
+?>
